3350712eb80be38fac587a72be22cf05abf118d8
Ivan K <Fox13613@gmail.com>
2025-10-03 18:16:03 +0200

    cat walked

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -6,35 +6,63 @@ const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
+const img = document.querySelector('img');
+
+// Функция для центрирования кошки по вертикали
+function centerVertically() {
+  img.style.top = `${(window.innerHeight - img.height) / 2}px`;
+}
+
+// Сразу центрируем и делаем адаптацию при изменении окна
+centerVertically();
+window.addEventListener('resize', centerVertically);
+
+// Начальная позиция слева за экраном
+img.style.left = `-${img.width}px`;
+
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Resolve this promise when the cat (`img`) has walked from `startPos` to
-    // `stopPos`.
-    // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
-    // constants.
+    let pos = startPos;
+    img.style.left = `${pos}px`;
+    
+    const interval = setInterval(() => {
+      if (pos >= stopPos) {
+        clearInterval(interval);
+        resolve(); // промис завершён
+      } else {
+        pos += STEP_SIZE_PX;
+        img.style.left = `${pos}px`;
+      }
+    }, STEP_INTERVAL_MS);
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Switch the `.src` of the `img` from the walking cat to the dancing cat
-    // and, after a timeout, reset the `img` back to the walking cat. Then
-    // resolve the promise.
-    // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+    const originalSrc = img.src;
+    img.src = DANCING_CAT_URL;
+    
+    setTimeout(() => {
+      img.src = originalSrc;
+      resolve(); // промис завершён
+    }, DANCE_TIME_MS);
   });
 }
 
 function catWalk() {
-  const img = document.querySelector('img');
   const startPos = -img.width;
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use the `walk()` and `dance()` functions to let the cat do the following:
-  // 1. Walk from `startPos` to `centerPos`.
-  // 2. Then dance for 5 secs.
-  // 3. Then walk from `centerPos` to `stopPos`.
-  // 4. Repeat the first three steps indefinitely.
+  function loop() {
+    walk(img, startPos, centerPos)
+      .then(() => dance(img))
+      .then(() => walk(img, centerPos, stopPos))
+      .then(loop); // бесконечный цикл через промисы
+  }
+
+  loop(); // запускаем цикл один раз
 }
 
+// Ждём полной загрузки страницы
 window.addEventListener('load', catWalk);
